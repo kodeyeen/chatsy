380adb774e5a3031f1d9dfbf6ce8199bd8248d65
iscanderoff <scanderoff@gmail.com>
2024-07-28 22:06:23 +0800

    Remove PasswordHash field from GetUserResponse

--- restapi/user.go
+++ restapi/user.go
@@ -3,11 +3,10 @@ package restapi
 import "time"
 
 type GetUserResponse struct {
-	ID           *int       `json:"id"`
-	Username     *string    `json:"username"`
-	FirstName    *string    `json:"firstName"`
-	LastName     *string    `json:"lastName"`
-	Email        *string    `json:"email"`
-	PasswordHash *string    `json:"-"`
-	JoinedAt     *time.Time `json:"joinedAt"`
+	ID        *int       `json:"id"`
+	Username  *string    `json:"username"`
+	FirstName *string    `json:"firstName"`
+	LastName  *string    `json:"lastName"`
+	Email     *string    `json:"email"`
+	JoinedAt  *time.Time `json:"joinedAt"`
 }
