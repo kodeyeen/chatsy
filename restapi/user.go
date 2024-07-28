a4694c69875156748bf1e58b5224b21fd4c66dd6
iscanderoff <scanderoff@gmail.com>
2024-07-28 22:05:01 +0800

    Remove GetUserResponse struct tags

--- restapi/user.go
+++ restapi/user.go
@@ -3,11 +3,11 @@ package restapi
 import "time"
 
 type GetUserResponse struct {
-	ID           *int       `json:"id" db:"id"`
-	Username     *string    `json:"username" db:"username"`
-	FirstName    *string    `json:"firstName" db:"first_name"`
-	LastName     *string    `json:"lastName" db:"last_name"`
-	Email        *string    `json:"email" db:"email"`
-	PasswordHash *string    `json:"-" db:"password_hash"`
-	JoinedAt     *time.Time `json:"joinedAt" db:"joined_at"`
+	ID           *int       `json:"id"`
+	Username     *string    `json:"username"`
+	FirstName    *string    `json:"firstName"`
+	LastName     *string    `json:"lastName"`
+	Email        *string    `json:"email"`
+	PasswordHash *string    `json:"-"`
+	JoinedAt     *time.Time `json:"joinedAt"`
 }
